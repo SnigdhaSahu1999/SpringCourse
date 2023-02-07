536cbd941aa64f6ee842aec44d15c4e78f0ee3ee
Snigdha <snigdha.sahu1999@gmail.com>
2023-02-07 22:37:42 +0530

    Second Commit

--- spring_demo_one/src/com/snigdha/springdemo/Coach.java
+++ spring_demo_one/src/com/snigdha/springdemo/Coach.java
@@ -2,5 +2,10 @@ package com.snigdha.springdemo;
 
 public interface Coach {
 	
+	//IOC Example
 	public String getDailyWorkout();
+	
+	//Dependency Injection Example
+	public String getDailyFortune();
+	
 }
--- spring_demo_one/src/com/snigdha/springdemo/HelloSpringApp.java
+++ spring_demo_one/src/com/snigdha/springdemo/HelloSpringApp.java
@@ -10,14 +10,22 @@ public class HelloSpringApp {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// retrieve bean from spring container
-		Coach theTrackCoach = context.getBean("myCoach",Coach.class);
+		//Coach theTrackCoach = context.getBean("myCoach",Coach.class);
 		
-		Coach theBaseballCoach = context.getBean("myCoach1",Coach.class);
+		//Coach theBaseballCoach = context.getBean("myCoach1",Coach.class);
+		
+		//Coach theBadmintonCoach = context.getBean("myCoach2",Coach.class);
 		
 		// call methods on the bean
-		System.out.println(theTrackCoach.getDailyWorkout());
-		System.out.println(theBaseballCoach.getDailyWorkout());
+		//System.out.println(theTrackCoach.getDailyWorkout());
+		//System.out.println(theBaseballCoach.getDailyWorkout());
+		//System.out.println(theBadmintonCoach.getDailyWorkout());
 		
+		
+		//Call methods for Dependency Injection Demonstration
+		Coach myTrackCoach = context.getBean("myTCoach",Coach.class);
+		System.out.println(myTrackCoach.getDailyWorkout());
+		System.out.println(myTrackCoach.getDailyFortune());
 		// close the context
 		context.close();
 	}
