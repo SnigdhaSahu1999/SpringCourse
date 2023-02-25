59b3be14985a5df228d4ad2dd00b95e290580101
Snigdha <snigdha.sahu1999@gmail.com>
2023-02-25 22:09:01 +0530

    AssignmentCoach.java added

---
+++ sprind_demo_annotations/src/com/snigdha/springdemo/AssignmentCoach.java
@@ -0,0 +1,26 @@
+package com.snigdha.springdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AssignmentCoach implements Coach {
+
+	@Autowired
+	@Qualifier("fileFortuneService")
+	private FortuneService fortuneService;
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return ("Complete assignments on daily basis!");
+	}
+
+	@Override
+	public String getDailyFortune() {
+	
+		return fortuneService.getFortune();
+	}
+
+}
