f8e472b5c50e89b1c3063359e53ffbe74e00b4cc
Snigdha <snigdha.sahu1999@gmail.com>
2023-03-01 23:59:30 +0530

    FileFortuneService.java added

--- sprind_demo_annotations/src/com/snigdha/springdemo/FileFortuneService.java
+++ sprind_demo_annotations/src/com/snigdha/springdemo/FileFortuneService.java
@@ -4,44 +4,47 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.stereotype.Component;
+
+/*Assignment 5*/
 
 @Component
 class FileFortuneService implements FortuneService {
 	
 	
 	private Random myRandom = new Random();
+	private List<String> arr;
+	private String directory = "C:\\Users\\SNIGDHA SAHU\\eclipse-workspace\\sprind_demo_annotations\\src\\com\\snigdha\\springdemo\\fortune-data.txt";
 
 	@Override
 	public String getFortune() {
 		
-		String arr[] = new String[4];
-		File file = new File("C:\\Users\\SNIGDHA SAHU\\eclipse-workspace\\sprind_demo_annotations\\src\\com\\snigdha\\springdemo\\dependencyInjectionWithAnnotations.txt");
-	 
-		 try {
-	        BufferedReader br = new BufferedReader(new FileReader(file));
-	 
+		File file = new File(directory);
+		arr = new ArrayList<String>();
+		 try (BufferedReader br = new BufferedReader(new FileReader(file))){
+	       
 	        // Declaring a string variable
 	        String st;
 	        // Condition holds true till
 	        // there is character in a string
 	        	
-	        	int c = 0;
-				while ((st = br.readLine()) != null)
+				while ((st = br.readLine()) != null) {
  
-				    // Print the string
-					arr[c] = st;
-					c += 1;
+				    arr.add(st);
+				}
 
 			} catch (IOException e) {
 				
 				System.out.println("IOException "+e);
 			}
 		 
-		 int index = myRandom.nextInt(arr.length);
-		 return(arr[index]);
+		 
+		 int index = myRandom.nextInt(arr.size());
+		 return(arr.get(index));
 		 
 		
 	}
