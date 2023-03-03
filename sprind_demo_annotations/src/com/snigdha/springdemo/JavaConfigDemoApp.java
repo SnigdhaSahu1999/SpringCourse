3bfffbf9428d1ba4a22bc00950f13b9fea3f59f6
Snigdha <snigdha.sahu1999@gmail.com>
2023-03-03 20:13:01 +0530

    JavaConfigDemoApp.java added

---
+++ sprind_demo_annotations/src/com/snigdha/springdemo/JavaConfigDemoApp.java
@@ -0,0 +1,27 @@
+package com.snigdha.springdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
+public class JavaConfigDemoApp {
+
+	public static void main(String[] args) {
+		
+		//read spring config file
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		
+		//get the bean(object)  from spring container
+		//"tennisCoach" id is created automatically bcoz we are usimg @Component in TennisCoach class
+		Coach theCoach = context.getBean("tennisCoach",Coach.class);
+		
+
+		System.out.println(theCoach.getDailyWorkout());
+		
+		System.out.println(theCoach.getDailyFortune());
+		
+		//close the context
+		context.close();
+	}
+
+}
