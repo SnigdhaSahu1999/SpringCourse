4115365b9c4cad261a88986356c61bb3bc4ddafa
Snigdha <snigdha.sahu1999@gmail.com>
2023-03-27 00:45:19 +0530

    AssignmentCoach.java added

--- sprind_demo_annotations/src/com/snigdha/springdemo/AssignmentCoach.java
+++ sprind_demo_annotations/src/com/snigdha/springdemo/AssignmentCoach.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+/*Assignment 5*/
 @Component
 public class AssignmentCoach implements Coach {
 
